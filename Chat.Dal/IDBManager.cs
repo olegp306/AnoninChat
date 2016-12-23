09042da2cf531ee26e04cb226bd0061995adf7cf
olegp306 <olegp306@gmail.com>
2016-12-23 16:35:55 +0700

    add intefgase for dbManager

---
+++ Chat.Dal/IDBManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Dal
+{
+    interface IDBManager
+    {
+
+        IEnumerable<Chat> GetAllChatsByUserId(string userId);
+
+        IEnumerable<Message> GetAllMessagesByChatId(Guid? chatId);
+
+        IEnumerable<ParticipantsChat> GetAllParticipantsChatByChatId(Guid? chatId);
+
+        ChatInstance SetChat(ChatInstance chat);
+
+        Message SetMessage(Message message);
+
+        ParticipantsChat SetParticipantsChat(ParticipantsChat participantsChat);
+    }
+}
